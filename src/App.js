f489c7db78212a4374fc1384e9beb431e64020ba
Kristi <kristiana.d.ingco@gmail.com>
2023-05-06 18:25:57 -0700

    Add React Router

--- src/App.js
+++ src/App.js
@@ -1,39 +1,12 @@
-import "./Categories.scss";
-import Directory from "./components/Directory/Directory"
+import { Routes, Route } from "react-router-dom";
+import Home from "./routes/Home/Home";
 
 const App = () => {
-  const categories = [
-    {
-      id: 1,
-      title: "Cleansers",
-      imageUrl:
-        "https://cdn.shopify.com/s/files/1/0111/9309/0106/products/00b32685030514f143ea4bed31742cb8_800x.jpg?v=1586810329",
-    },
-    {
-      id: 2,
-      title: "Toners",
-      imageUrl:
-        "https://m.media-amazon.com/images/I/21iXuOPrmZL.jpg",
-    },
-    {
-      id: 3,
-      title: "Serums",
-      imageUrl: "https://m.media-amazon.com/images/I/51D0W7wjXGL.jpg",
-    },
-    {
-      id: 4,
-      title: "Lotions",
-      imageUrl:
-        "https://cafe24img.poxo.com/beautyboxkorea/web/product/big/ROUND-LAB-1025-Dokdo-Cream-80ml.jpg",
-    },
-    {
-      id: 5,
-      title: "Sunscreen",
-      imageUrl: "https://m.media-amazon.com/images/I/31gK4s1-otL.jpg",
-    },
-  ];
-
-  return <Directory categories={categories}/>;
+  return (
+    <Routes>
+      <Route path='/' element={<Home />} />
+    </Routes>
+  );
 };
 
 export default App;
---
+++ src/routes/Home/Home.jsx
@@ -0,0 +1,37 @@
+import Directory from "../../components/Directory/Directory";
+
+const Home = () => {
+  const categories = [
+    {
+      id: 1,
+      title: "Cleansers",
+      imageUrl:
+        "https://cdn.shopify.com/s/files/1/0111/9309/0106/products/00b32685030514f143ea4bed31742cb8_800x.jpg?v=1586810329",
+    },
+    {
+      id: 2,
+      title: "Toners",
+      imageUrl: "https://m.media-amazon.com/images/I/21iXuOPrmZL.jpg",
+    },
+    {
+      id: 3,
+      title: "Serums",
+      imageUrl: "https://m.media-amazon.com/images/I/51D0W7wjXGL.jpg",
+    },
+    {
+      id: 4,
+      title: "Lotions",
+      imageUrl:
+        "https://cafe24img.poxo.com/beautyboxkorea/web/product/big/ROUND-LAB-1025-Dokdo-Cream-80ml.jpg",
+    },
+    {
+      id: 5,
+      title: "Sunscreen",
+      imageUrl: "https://m.media-amazon.com/images/I/31gK4s1-otL.jpg",
+    },
+  ];
+
+  return <Directory categories={categories} />;
+};
+
+export default Home;
