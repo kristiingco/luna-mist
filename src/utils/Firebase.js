f583dd0ec369bacc76c772c501330e86e5be9b78
Kristi <kristiana.d.ingco@gmail.com>
2023-05-10 22:05:37 -0700

    Add creating new user logic

--- src/utils/Firebase.js
+++ src/utils/Firebase.js
@@ -31,4 +31,21 @@ export const db = getFirestore();
 export const createUserDocumentFromAuth = async (userAuth) => {
   const userDocRef = doc(db, "users", userAuth.uid);
   const userSnapshot = await getDoc(userDocRef);
+
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log("Error creating user", error.message);
+    }
+  }
+
+  return userDocRef;
 };
