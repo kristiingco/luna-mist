9a777762d796e9935803ba0a27c93e932af093f9
Kristi <kristiana.d.ingco@gmail.com>
2023-05-28 11:10:34 -0700

    Add listener for auth state change

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,4 +1,5 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
+import { onAuthStateChangedListener } from "../utils/Firebase";
 
 export const UserContext = createContext({
   setCurrentUser: () => null,
@@ -8,6 +9,12 @@ export const UserContext = createContext({
 export const UserProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState(null);
   const value = { currentUser, setCurrentUser };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChangedListener(setCurrentUser);
+
+    return unsubscribe;
+  }, []);
 
   return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
 };
