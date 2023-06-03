83ccaf472c6c705e38c715fe8690c8f11b9d3c52
Kristi <kristiana.d.ingco@gmail.com>
2023-06-03 15:19:53 -0700

    Create logic for adding products to cart

--- src/components/CartDropdown/CartDropdown.jsx
+++ src/components/CartDropdown/CartDropdown.jsx
@@ -1,11 +1,21 @@
-import "./CartDropdown.scss";
+import { useContext } from "react";
+
+import { CartContext } from "../../context/CartContext";
 
 import Button from "../Button/Button";
+import CartItem from "../CartItem/CartItem";
+
+import "./CartDropdown.scss";
 
 const CartDropdown = () => {
+  const { cartItems } = useContext(CartContext);
   return (
     <div className='cart-dropdown-container'>
-      <div className='cart-items'></div>
+      <div className='cart-items'>
+        {cartItems.map((item) => (
+          <CartItem key={item.id} cartItem={item} />
+        ))}
+      </div>
       <Button>CHECKOUT</Button>
     </div>
   );
